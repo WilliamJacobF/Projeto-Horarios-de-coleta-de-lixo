07e9f517f1229b54cf16519f9611d0d78c0f60a2
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2025-04-20 17:58:59 -0300

    finalizando a api

--- Backend/server-coleta/app/Http/Controllers/ApiController.php
+++ Backend/server-coleta/app/Http/Controllers/ApiController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Horario;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    
+    public function show($id){
+        return Horario::findOrFail($id);
+    }
 }
