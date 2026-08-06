5c5aa3037bfd3ca895227f83bd4d7e538ada5118
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2026-08-05 23:17:49 -0300

    sanctum

--- Backend/server-coleta/routes/api.php
+++ Backend/server-coleta/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/ruas', [ApiController::class, 'index']);
 Route::get('/ruas/buscar/{nome}', [ApiController::class, 'buscarPorNome']);
+Route::get('/login', [AuthController::class, 'login']);
