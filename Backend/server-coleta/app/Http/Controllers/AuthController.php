dcfb4ebc90fd9b0ff001bdd27405a5b4dd4ca8a1
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2026-08-06 22:46:53 -0300

    sanctum

--- Backend/server-coleta/app/Http/Controllers/AuthController.php
+++ Backend/server-coleta/app/Http/Controllers/AuthController.php
@@ -4,11 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => [required],
+        ]);
 
+        if (! Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Credenciais inválidas.'
+            ], 401);
+        }
+
+        $request->session()->regerate();
+
+        return response()->json([
+            'message' => 'Login realizado.'
+        ]);
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return response()->json([
+            'message' =>'logout realizado'
+        ]);
+    }
+
 }
