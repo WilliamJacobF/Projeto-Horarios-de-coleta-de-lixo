b51e947e7422264df518f30b6900a216419c6dd2
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2026-08-07 23:28:34 -0300

    sanctum

--- Backend/server-coleta/routes/api.php
+++ Backend/server-coleta/routes/api.php
@@ -8,4 +8,15 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/ruas', [ApiController::class, 'index']);
 Route::get('/ruas/buscar/{nome}', [ApiController::class, 'buscarPorNome']);
-Route::get('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::apiResource('horarios', ApiController::class);
+
+});
